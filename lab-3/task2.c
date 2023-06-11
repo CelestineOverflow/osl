9cd31f8e8527ccf8ee3282ef551cb3b40505dbf5
CelestineOverflow <77799581+CelestineOverflow@users.noreply.github.com>
2023-06-11 23:18:34 +0200

    osl lab fi

--- lab-3/task2.c
+++ lab-3/task2.c
@@ -8,7 +8,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#define MAX_TEXT 512
+#define MAX_TEXT 100
 
 struct msg_buffer
 {
@@ -16,47 +16,75 @@ struct msg_buffer
     char message_text[MAX_TEXT];
 } message;
 
-void readMessagueQueue(int msgid, struct msg_buffer *message, long type)
-{
-    msgrcv(msgid, message, MAX_TEXT, type, 0);
-}
-
-void writeMessageQueue(int msgid, struct msg_buffer *message)
-{
-    msgsnd(msgid, message, strlen(message->message_text) + 1, 0);
-}
-
 void writeTestProccess(int msgid, struct msg_buffer *message, char *text)
 {
     message->message_type = 1;
     strcpy(message->message_text, text);
-    writeMessageQueue(msgid, message);
+    msgsnd(msgid, message, strlen(message->message_text) + 1, 0);
 }
+
+#define __USE_BLOCKING
 
 void readTestProccess(int msgid, struct msg_buffer *message)
 {
-    readMessagueQueue(msgid, message, 1);
-    printf("Data received is: %s\n", message->message_text);
-    //remove message queue
-    msgctl(msgid, IPC_RMID, NULL);
+    #ifdef __USE_BLOCKING
+        //blocking until message arrive
+        msgrcv(msgid, message, MAX_TEXT, 1, 0);
+        printf("Data Received is : %s \n", message->message_text);
+    #endif
+
+    #ifdef __USE_NON_BLOCKING
+        //non blocking
+        if (msgrcv(msgid, message, MAX_TEXT, 1, IPC_NOWAIT) == -1)
+        {
+            printf("No data\n");
+        }
+        else
+        {
+            printf("Data Received is : %s \n", message->message_text);
+        }
+    #endif
 }
 
 int main(int argc, char **argv)
 {
     key_t key = ftok("/some/path", 65);        // create unique key
-    int msgid = msgget(key, 0666 | IPC_CREAT); // returns message queue identifier
+    //create message queue
+    int msgid = msgget(key, 0666 | IPC_CREAT | IPC_EXCL); // create message queue and return id
+    if (msgid == -1)
+    {
+        printf("Message queue already exist\n");
+        msgid = msgget(key, 0666);
+    }
+    else
+    {
+        printf("Message queue created\n");
+    }
     //create child process
     pid_t pid = fork();
     if (pid == 0)
     {
         //child process
-        //wait for 5 seconds
-        sleep(5);
-        readTestProccess(msgid, &message);
+        //read all message from message queue
+        for(int i = 0 ; i < 10 ; i++)
+        {
+            readTestProccess(msgid, &message);
+        }
     }
     else
     {
         //parent process
-        writeTestProccess(msgid, &message, "Hello world!");
+        //
+        sleep(5);
+        int times = atoi(argv[1]);
+        printf("times: %d\n", times);
+        for (int i = 0; i < times; i++)
+        {
+            printf("writing message to message queue %d\n", i);
+            char msg[MAX_TEXT];
+            sprintf(msg, "Hello from parent process %d", i);
+            writeTestProccess(msgid, &message, msg);
+        }
+        printf("Data has been sent\n");
     }
 }
--- lab-3/task3.c
+++ lab-3/task3.c
@@ -1,49 +1,189 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <unistd.h>
+// #include <sys/mman.h>
+// #include <sys/types.h>
+// #include <sys/wait.h>
+// #include <fcntl.h>
+// #include <pthread.h>
+
+// #define SHM_NAME "/shm_example"
+// #define MUTEX_NAME "/mutex_example"
+
+// typedef struct {
+//     pthread_mutex_t mutex;
+//     int value;
+// } shared_data;
+
+// int main() {
+//     int shm_fd;
+//     shared_data *sh_data;
+
+//     /* Create shared memory object and set its size */
+//     shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+//     if (shm_fd == -1) {
+//         perror("Shared memory");
+//         return -1;
+//     }
+
+//     if (ftruncate(shm_fd, sizeof(shared_data)) != 0) {
+//         perror("Shared memory size");
+//         return -1;
+//     }
+
+//     /* Map shared memory object */
+//     sh_data = (shared_data *)mmap(NULL, sizeof(shared_data), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+//     if (sh_data == MAP_FAILED) {
+//         perror("Memory mapping");
+//         return -1;
+//     }
+
+//     /* Initialize mutex */
+//     pthread_mutexattr_t attrmutex;
+//     pthread_mutexattr_init(&attrmutex);
+//     pthread_mutexattr_setpshared(&attrmutex, PTHREAD_PROCESS_SHARED);
+//     pthread_mutex_init(&sh_data->mutex, &attrmutex);
+
+//     pid_t pid = fork();
+
+//     if (pid < 0) {
+//         perror("Fork");
+//         return -1;
+//     } else if (pid == 0) { /* Child process */
+//         for (int i = 0; i < 10; ++i) {
+//             pthread_mutex_lock(&sh_data->mutex);
+//             if (sh_data->value != 0) {
+//                 printf("Child read: %d\n", sh_data->value);
+//                 sh_data->value = 0;
+//             }
+//             sh_data->value = i + 1;
+//             printf("Child wrote: %d\n", sh_data->value);
+//             pthread_mutex_unlock(&sh_data->mutex);
+//             sleep(1);
+//         }
+//     } else { /* Parent process */
+//         for (int i = 0; i < 10; ++i) {
+//             pthread_mutex_lock(&sh_data->mutex);
+//             if (sh_data->value != 0) {
+//                 printf("Parent read: %d\n", sh_data->value);
+//                 sh_data->value = 0;
+//             }
+//             sh_data->value = i + 1;
+//             printf("Parent wrote: %d\n", sh_data->value);
+//             pthread_mutex_unlock(&sh_data->mutex);
+//             sleep(1);
+//         }
+//         wait(NULL);
+//     }
+
+//     /* Clean up */
+//     munmap(sh_data, sizeof(shared_data));
+//     shm_unlink(SHM_NAME);
+
+//     return 0;
+// }
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-//include fork and wait
+#include <unistd.h>
+#include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <unistd.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <time.h>
 
-int main(int argc, char **argv)
-{
-    // ftok to generate unique key
-    key_t key = ftok("/some/path", 65);
+#define SHM_NAME "/shm_example"
+#define MUTEX_NAME "/mutex_example"
 
-    // shmget returns an identifier in shmid
-    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+typedef struct {
+    pthread_mutex_t mutex;
+    size_t block_size;
+    char data[];
+} shared_data;
 
-    // shmat to attach to shared memory
-    char *str = (char *)shmat(shmid, (void *)0, 0);
+int main() {
+    int shm_fd;
+    shared_data *sh_data;
+    size_t block_sizes[] = { 1024, 2048, 4096, 8192 , 16384, 32768, 65536, 131072, 262144, 524288 };
+    size_t num_sizes = sizeof(block_sizes) / sizeof(size_t);
+    char *temp_buf;
+    struct timespec start, end;
+    long long elapsed_ns;
 
-    // create child process
-    pid_t pid = fork();
-    if (pid == 0)
-    {
-        // child process
-        // wait for 5 seconds
-        sleep(5);
-        printf("Data read from memory: %s\n", str);
+    for (size_t i = 0; i < num_sizes; ++i) {
+        size_t size = block_sizes[i];
 
-        //detach from shared memory
-        shmdt(str);
+        /* Create shared memory object and set its size */
+        shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+        if (shm_fd == -1) {
+            perror("Shared memory");
+            return -1;
+        }
 
-        // destroy the shared memory
-        shmctl(shmid, IPC_RMID, NULL);
+        if (ftruncate(shm_fd, sizeof(shared_data) + size) != 0) {
+            perror("Shared memory size");
+            return -1;
+        }
+
+        /* Map shared memory object */
+        sh_data = (shared_data *)mmap(NULL, sizeof(shared_data) + size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+        if (sh_data == MAP_FAILED) {
+            perror("Memory mapping");
+            return -1;
+        }
+
+        /* Initialize mutex */
+        pthread_mutexattr_t attrmutex;
+        pthread_mutexattr_init(&attrmutex);
+        pthread_mutexattr_setpshared(&attrmutex, PTHREAD_PROCESS_SHARED);
+        pthread_mutex_init(&sh_data->mutex, &attrmutex);
+
+        /* Allocate temp buffer */
+        temp_buf = malloc(size);
+        memset(temp_buf, 0, size);
+
+        pid_t pid = fork();
+
+        if (pid < 0) {
+            perror("Fork");
+            return -1;
+        } else if (pid == 0) { /* Child process */
+            clock_gettime(CLOCK_REALTIME, &start);
+            pthread_mutex_lock(&sh_data->mutex);
+            memcpy(sh_data->data, temp_buf, size);
+            pthread_mutex_unlock(&sh_data->mutex);
+            clock_gettime(CLOCK_REALTIME, &end);
+            elapsed_ns = (end.tv_sec - start.tv_sec) * 1000000000LL + (end.tv_nsec - start.tv_nsec);
+            printf("Child wrote %zu bytes in %lld ns\n", size, elapsed_ns);
+            exit(0);
+        } else { /* Parent process */
+            wait(NULL); /* Wait for child to finish writing */
+            clock_gettime(CLOCK_REALTIME, &start);
+            pthread_mutex_lock(&sh_data->mutex);
+            memcpy(temp_buf, sh_data->data, size);
+            pthread_mutex_unlock(&sh_data->mutex);
+            clock_gettime(CLOCK_REALTIME, &end);
+            elapsed_ns = (end.tv_sec - start.tv_sec) * 1000000000LL + (end.tv_nsec - start.tv_nsec);
+            printf("Parent read %zu bytes in %lld ns\n", size, elapsed_ns);
+        }
+
+        /* Clean up */
+        munmap(sh_data, sizeof(shared_data) + size);
+
+        if (shm_unlink(SHM_NAME) != 0) {
+            perror("Unlink shared memory");
+            return -1;
+        }
+
+        free(temp_buf);
     }
-    else
-    {
-        // parent process
-        printf("Write Data : ");
-        fgets(str, 100, stdin);
-        printf("Data written in memory: %s\n", str);
 
-        //detach from shared memory
-        shmdt(str);
-    }
 
-    return 0;
+    return 0;   
 }
+
+       
